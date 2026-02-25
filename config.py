55c4b404eea69f18e20fb667a7648095bda8bd47
MAmIoT <mamiot.lab@gmail.com>
2026-02-25 12:41:58 +0100

    Initial Notebooks Configured

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-DATA_ROOT = "/home/server/EPIC-KITCHENS"
-ANNOTATION_CSV = f"{DATA_ROOT}/annotations/EPIC_train_object_labels.csv"
+FRAMES_ROOT = "/home/server/EPIC-KITCHENS"
+DATA_ROOT = "../annotations"
+ANNOTATION_CSV = f"{DATA_ROOT}/EPIC_train_object_labels.csv"
 OUTPUT_DIR = "./data/epic_train_subset"
 CLASSES = ["bread", "knife", "cheese", "ham", "tomato", "cucumber", "carrot", "butter"]
+CLASS_MAPPING = {
+    "bread": ["bread", "bread package", "bread packaging"],
+    "knife": ["knife", "mezzaluna knife", "mincing knife"]
+}
 MAX_PER_CLASS = 1000
 TRAIN_SPLIT = 0.9
 IMG_EXT = ".jpg"
