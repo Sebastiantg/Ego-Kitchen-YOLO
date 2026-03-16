4277f94b6bd0409d54230110bfc5c9de660b17e8
Sebastiantg <sebastian.brawl@hotmail.com>
2026-03-16 10:34:18 +0100

    Added ground thruths and IoU calculation to the dataset generation notebook. Also added a requirements.txt file for easier environment setup.

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
-FRAMES_ROOT = "/home/server/EPIC-KITCHENS"
-DATA_ROOT = "../annotations"
-ANNOTATION_CSV = f"{DATA_ROOT}/EPIC_train_object_labels.csv"
-OUTPUT_DIR = "./data/epic_train_subset"
-CLASSES = ["bread", "knife", "cheese", "ham", "tomato", "cucumber", "carrot", "butter"]
+DATASET_ROOT = "/nfs/datasets/EPIC-KITCHENS"
+ANNOTATIONS_ROOT = f"{DATASET_ROOT}/annotations"
+TRAIN_OBJECT_LABELS_CSV = f"{ANNOTATIONS_ROOT}/EPIC_train_object_labels.csv"
+EPIC_100_TRAIN_CSV = f"{ANNOTATIONS_ROOT}/EPIC_100_train.csv"
+EPIC_100_NOUN_CLASSES_CSV = f"{ANNOTATIONS_ROOT}/EPIC_100_noun_classes_v2.csv"
+OUTPUT_DIR = "/nfs/workspace/sebastian.toro/EPIC-KITCHENS/Ego-Kitchen-YOLO/notebooks/data/epic_train_subset"
 CLASS_MAPPING = {
     "bread": ["bread", "bread package", "bread packaging"],
-    "knife": ["knife", "mezzaluna knife", "mincing knife"]
+    "knife": ["knife", "mezzaluna knife", "mincing knife"],
+    "cheese": ["cheese", "cheese slices"],
+    "meat": ["ham", "meat", "chicken", "bacon", "sausage"],
+    "tomato": ["tomato", "tomatoes", "cherry tomato", "cherry tomatoes"],
+    "vegetables": ["cucumber", "courgette", "zucchini", "lettuce", "pepper", "onion"],
+    "carrot": ["carrot", "carrots", "carrot peelings"],
+    "sauce": ["butter", "mayonnaise", "ketchup", "mustard", "sauce"]
 }
+
+CLASSES = [*CLASS_MAPPING]
+
 MAX_PER_CLASS = 1000
 TRAIN_SPLIT = 0.9
 IMG_EXT = ".jpg"
